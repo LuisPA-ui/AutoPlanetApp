1ff14f7f67dd08d9976a58d69cf1e7a0004005de
Alexander <u201420866@upc.edu.pe>
2019-11-08 22:52:20 -0500

    creacion de servicio producto

--- src/main/java/pe/edu/upc/autoplanet/service/ProductService.java
+++ src/main/java/pe/edu/upc/autoplanet/service/ProductService.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import pe.edu.upc.autoplanet.model.Product;
 
 public interface ProductService {
+	
 	public abstract int createProduct(Product product);
 	public abstract int updateProduct(Long id, Product product);
 	public abstract int deleteProduct(Long id);
