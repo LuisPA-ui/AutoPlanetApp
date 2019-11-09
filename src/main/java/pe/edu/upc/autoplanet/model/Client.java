1c7a56876c7b6ccd7f1368596c22e68a40b829df
Alexander <u201420866@upc.edu.pe>
2019-11-08 22:29:55 -0500

    creacion de la entidad cliente

--- src/main/java/pe/edu/upc/autoplanet/model/Client.java
+++ src/main/java/pe/edu/upc/autoplanet/model/Client.java
@@ -6,10 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
-@Table(name = "clients")
+@Table(name = "clients" , uniqueConstraints = { @UniqueConstraint(columnNames = { "dni" }) })
 public class Client {
 	
 	public Long getId() {
